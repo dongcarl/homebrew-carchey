a932fe18db715fb4ffca6942374cce42d3b7c68e
Carl Dong <dongcarl@fastmail.fm>
2016-01-19 12:02:53 -0800

    Update carchey.rb

--- carchey.rb
+++ carchey.rb
@@ -2,7 +2,7 @@ class Carchey < Formula
   desc "Archey script clone for OS X written in C"
   homepage "https://github.com/dongcarl/carchey"
   url "https://github.com/dongcarl/carchey/archive/v1.0.tar.gz"
-  sha256 "6e9a4060134d8d5057071925ab77bf492748b275b66b72ed1ef7d2ede456404f"
+  sha256 "485275b6178a4b1799d3f40de667d534cbc5b0eea7a61803f90f2a4bc4888fe8"
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
